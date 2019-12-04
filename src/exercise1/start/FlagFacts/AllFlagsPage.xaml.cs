5c18d4b8e6a2a0231c06633ab752eab036cfe367
larencloete <56066363+larencloete@users.noreply.github.com>
2019-12-04 09:57:06 +0200

    second commit

---
+++ src/exercise1/start/FlagFacts/AllFlagsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using FlagData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlagFacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlagsPage : ContentPage
+    {
+        public AllFlagsPage()
+        {
+            BindingContext = DependencyService.Get<FlagDetailsViewModel>();
+            InitializeComponent();
+        }
+
+        async void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await this.Navigation.PushAsync(new FlagDetailsPage());
+        }
+    }
+}
